4f7638cef3f55d4d5f59cfd2fa972176505fc68b
shady samara <shadysamara@shadys-MacBook-Pro.local>
2021-12-07 16:10:16 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,228 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:bluetooth_print/bluetooth_print.dart';
+import 'package:bluetooth_print/bluetooth_print_model.dart';
+import 'package:charset_converter/charset_converter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  BluetoothPrint bluetoothPrint = BluetoothPrint.instance;
+
+  bool _connected = false;
+  BluetoothDevice _device;
+  String tips = 'no device connect';
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => initBluetooth());
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initBluetooth() async {
+    bluetoothPrint.startScan(timeout: Duration(seconds: 4));
+
+    bool isConnected = await bluetoothPrint.isConnected;
+
+    bluetoothPrint.state.listen((state) {
+      print('cur device status: $state');
+
+      switch (state) {
+        case BluetoothPrint.CONNECTED:
+          setState(() {
+            _connected = true;
+            tips = 'connect success';
+          });
+          break;
+        case BluetoothPrint.DISCONNECTED:
+          setState(() {
+            _connected = false;
+            tips = 'disconnect success';
+          });
+          break;
+        default:
+          break;
+      }
+    });
+
+    if (!mounted) return;
+
+    if (isConnected) {
+      setState(() {
+        _connected = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('BluetoothPrint example app'),
+        ),
+        body: RefreshIndicator(
+          onRefresh: () =>
+              bluetoothPrint.startScan(timeout: Duration(seconds: 4)),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Image.asset('assets/images/flutter_shady.jpg'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                      child: Text(tips),
+                    ),
+                  ],
+                ),
+                Divider(),
+                StreamBuilder<List<BluetoothDevice>>(
+                  stream: bluetoothPrint.scanResults,
+                  initialData: [],
+                  builder: (c, snapshot) => Column(
+                    children: snapshot.data
+                        .map((d) => ListTile(
+                              title: Text(d.name ?? ''),
+                              subtitle: Text(d.address),
+                              onTap: () async {
+                                setState(() {
+                                  _device = d;
+                                });
+                              },
+                              trailing: _device != null &&
+                                      _device.address == d.address
+                                  ? Icon(
+                                      Icons.check,
+                                      color: Colors.green,
+                                    )
+                                  : null,
+                            ))
+                        .toList(),
+                  ),
+                ),
+                Divider(),
+                Container(
+                  padding: EdgeInsets.fromLTRB(20, 5, 20, 10),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          OutlinedButton(
+                            child: Text('connect'),
+                            onPressed: _connected
+                                ? null
+                                : () async {
+                                    if (_device != null &&
+                                        _device.address != null) {
+                                      await bluetoothPrint.connect(_device);
+                                    } else {
+                                      setState(() {
+                                        tips = 'تجربة محتوى عربي';
+                                      });
+                                      print('please select device');
+                                    }
+                                  },
+                          ),
+                          SizedBox(width: 10.0),
+                          OutlinedButton(
+                            child: Text('disconnect'),
+                            onPressed: _connected
+                                ? () async {
+                                    await bluetoothPrint.disconnect();
+                                  }
+                                : null,
+                          ),
+                        ],
+                      ),
+                      OutlinedButton(
+                        child: Text('طباعة نص'),
+                        onPressed: _connected
+                            ? () async {
+                                Map<String, dynamic> config = Map();
+                                config['code_page'] = 22;
+                                List<LineText> list = List();
+                                // ByteData data = await rootBundle
+                                //     .load("assets/images/flutter_shady.jpg");
+                                // List<int> imageBytes = data.buffer.asUint8List(
+                                //     data.offsetInBytes, data.lengthInBytes);
+                                // String base64Image = base64Encode(imageBytes);
+                                // list.add(LineText(
+                                //     type: LineText.TYPE_IMAGE,
+                                //     content: base64Image,
+                                //     align: LineText.ALIGN_CENTER,
+                                //     linefeed: 1));
+                                // Uint8List encArabic =
+                                //     await CharsetConverter.encode(
+                                //         "windows-1256",
+                                //         "نص عربي بس الله الرحمن الرحيم");
+
+                                list.add(LineText(
+                                    type: LineText.TYPE_TEXT,
+                                    content: 'نص عربي بس الله الرحمن الرحيم',
+                                    weight: 1,
+                                    align: LineText.ALIGN_CENTER,
+                                    linefeed: 1));
+
+                                // list.add(LineText(linefeed: 1));
+                                // list.add(LineText(
+                                //     type: LineText.TYPE_BARCODE,
+                                //     content: 'A12312112',
+                                //     size: 10,
+                                //     align: LineText.ALIGN_CENTER,
+                                //     linefeed: 1));
+                                // list.add(LineText(linefeed: 1));
+                                // list.add(LineText(
+                                //     type: LineText.TYPE_QRCODE,
+                                //     content: 'qrcode i',
+                                //     size: 10,
+                                //     align: LineText.ALIGN_CENTER,
+                                //     linefeed: 1));
+                                // list.add(LineText(linefeed: 1));
+                                await bluetoothPrint.printReceipt(config, list);
+                              }
+                            : null,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: StreamBuilder<bool>(
+          stream: bluetoothPrint.isScanning,
+          initialData: false,
+          builder: (c, snapshot) {
+            if (snapshot.data) {
+              return FloatingActionButton(
+                child: Icon(Icons.stop),
+                onPressed: () => bluetoothPrint.stopScan(),
+                backgroundColor: Colors.red,
+              );
+            } else {
+              return FloatingActionButton(
+                  child: Icon(Icons.search),
+                  onPressed: () =>
+                      bluetoothPrint.startScan(timeout: Duration(seconds: 4)));
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
